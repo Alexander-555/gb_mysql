79bbb425c3c935f8cc87646ffdf36cbf41c3ac95
Alexander-555 <alexander-555@mail.ru>
2021-06-24 17:32:28 +0300

    Update create_db_example.sql

--- lesson2/create_db_example.sql
+++ lesson2/create_db_example.sql
@@ -2,4 +2,7 @@ CREATE DATABASE IF NOT EXISTS example;
 
 USE example;
 
-CREATE TABLE IF NOT EXISTS users (id int, name char[20]); 
+CREATE TABLE IF NOT EXISTS users (
+	id SERIAL PRIMARY KEY, 
+	name VARCHAR(255) COMMENT 'Имя'
+);
