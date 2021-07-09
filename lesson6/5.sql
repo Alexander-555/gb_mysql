fe3397800b3d712f3898cf6a86033098345b9b4b
Alexander-555 <alexander-555@mail.ru>
2021-07-09 18:17:36 +0300

    Update 5.sql

--- lesson6/5.sql
+++ lesson6/5.sql
@@ -5,12 +5,12 @@
 
 SELECT 
 	id as user_id,
-    CONCAT(
+	CONCAT(
 		(SELECT firstname FROM `profile` WHERE user_id = `user`.id),
-        ' ',
-        (SELECT lastname FROM `profile` WHERE user_id = `user`.id)
-    ) as full_name,
-    (SELECT count(*) FROM message WHERE from_user_id = `user`.id OR to_user_id = `user`.id) +
+        	' ',
+        	(SELECT lastname FROM `profile` WHERE user_id = `user`.id)
+    	) as full_name,
+    	(SELECT count(*) FROM message WHERE from_user_id = `user`.id OR to_user_id = `user`.id) +
 		(SELECT count(*) FROM `like` WHERE user_id = `user`.id) +
 		(SELECT count(*) FROM `post` WHERE user_id = `user`.id) +
 		(SELECT count(*) FROM `media` WHERE user_id = `user`.id) 
