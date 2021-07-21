c72dde1057a958e8f76297260144b83327cff5db
Alexander-555 <alexander-555@mail.ru>
2021-07-21 13:12:24 +0300

    Create 2.sql

---
+++ lesson7/2.sql
@@ -0,0 +1,10 @@
+SELECT 
+	products.name as `Товар`,
+    products.description as `Описание товара`,
+    catalogs.name as `Каталог`
+FROM
+	products
+LEFT JOIN 
+	catalogs
+ON
+	products.catalog_id = catalogs.id;
