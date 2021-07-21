987ae2210ad6807793c4f5c609822c957bff2ae3
Alexander-555 <alexander-555@mail.ru>
2021-07-21 13:33:15 +0300

    Create 3.sql

---
+++ lesson7/3.sql
@@ -0,0 +1,15 @@
+SELECT 
+	id,
+	c1.name as `Откуда`,
+    c2.name as `Куда`
+FROM 
+	flights
+JOIN 
+	cities as c1
+ON 
+	`from` = c1.`label`
+JOIN 
+	cities as c2
+ON 
+	`to` = c2.`label`
+ORDER BY id;
