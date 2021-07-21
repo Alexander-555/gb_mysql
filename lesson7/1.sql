fd71059bfb5a47b5e8f426ba4ed6150d13f57c42
Alexander-555 <alexander-555@mail.ru>
2021-07-21 12:50:19 +0300

    Update 1.sql

--- lesson7/1.sql
+++ lesson7/1.sql
@@ -1,1 +1,11 @@
-SELECT users.id, users.name FROM users INNER JOIN orders ON users.id = orders.user_id GROUP BY users.id;
+SELECT 
+  users.id, 
+  users.name 
+FROM 
+  users 
+INNER JOIN 
+  orders 
+ON 
+  users.id = orders.user_id 
+GROUP BY 
+  users.id;
