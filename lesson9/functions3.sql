910856e1ff109b0acd42807c34f6c6c07aa87f2c
Alexander-555 <alexander-555@mail.ru>
2021-07-27 16:37:49 +0300

    Update functions3.sql

--- lesson9/functions3.sql
+++ lesson9/functions3.sql
@@ -5,14 +5,14 @@ RETURNS int DETERMINISTIC
 BEGIN
     RETURN 
     (
-		WITH RECURSIVE fibonacci (n, fib_n, next_fib_n) AS
-		    (
-		      SELECT 1, 0, 1
-		      UNION ALL
-		      SELECT n + 1, next_fib_n, fib_n + next_fib_n
-			    FROM fibonacci WHERE n < 10
-		    )
-		    SELECT next_fib_n FROM fibonacci WHERE n = digit
+	WITH RECURSIVE fibonacci (n, fib_n, next_fib_n) AS
+	    (
+	      SELECT 1, 0, 1
+	      UNION ALL
+	      SELECT n + 1, next_fib_n, fib_n + next_fib_n
+		    FROM fibonacci WHERE n < 10
+	    )
+	    SELECT next_fib_n FROM fibonacci WHERE n = digit
     );
 END//
 
