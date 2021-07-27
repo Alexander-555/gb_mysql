d3736934c615f7e030313a81033dccad671bc8d3
Alexander-555 <alexander-555@mail.ru>
2021-07-27 11:50:35 +0300

    Update transaction3.sql

--- lesson9/transaction3.sql
+++ lesson9/transaction3.sql
@@ -1,14 +1,16 @@
+--  календарное поле created_at содержится в таблице dates
+
 SET @r := 0;
 
 SELECT 
-	IF(i.day < 10, concat("2018-08-0", i.day), concat("2018-08-", i.day)) AS date,
+    IF(i.day < 10, concat("2018-08-0", i.day), concat("2018-08-", i.day)) AS date,
     (SELECT created_at FROM dates WHERE created_at = IF(i.day < 10, concat("2018-08-0", i.day), concat("2018-08-", i.day))) IS NOT NULL AS is_present
 FROM
-	(
-		SELECT @r := @r + 1 AS `day` 
+    (
+        SELECT @r := @r + 1 AS `day` 
         FROM 
-			(select 1 union select 2 union select 3 union select 4) t1,
-			(select 1 union select 2 union select 3 union select 4) t2,
-			(select 1 union select 2 union select 3 union select 2) t3
-		LIMIT 31
-	) AS i;
+	    (select 1 union select 2 union select 3 union select 4) t1,
+	    (select 1 union select 2 union select 3 union select 4) t2,
+	    (select 1 union select 2 union select 3 union select 2) t3
+	LIMIT 31
+    ) AS i;
